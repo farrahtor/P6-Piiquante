e232a7cc1166c558b1ddbb06b0e3e68d00375776
farrahtor <farrahtor@gmail.com>
2022-12-15 12:56:20 +0100

    correction modify

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -26,24 +26,12 @@ exports.modifySauce = (req, res, next) => {
         }`,
       }
     : { ...req.body };
-
-  delete sauceObject._userId;
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: "Not authorized" });
-      } else {
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { ...sauceObject, _id: req.params.id }
-        )
-          .then(() => res.status(200).json({ message: "Sauce modifié!" }))
-          .catch((error) => res.status(401).json({ error }));
-      }
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+  Sauce.updateOne(
+    { _id: req.params.id },
+    { ...sauceObject, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Sauce modifié!" }))
+    .catch((error) => res.status(401).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
